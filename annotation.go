b68ac2d7b5cb562e766463fa44aa4843f3a393db
c9s <yoanlin93@gmail.com>
2017-11-13 20:31:08 +0800

    move annotation package to src/annotation
    Former-commit-id: 27e93e2759567d4c77063a2009129a70384afa41

---
+++ annotation.go
@@ -0,0 +1,132 @@
+package annotation
+
+import "bitbucket.org/linkernetworks/cv-tracker/server/parser/pts"
+import "strconv"
+import "math"
+import "image"
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+type PointAnnotation struct {
+	Label string `bson:"label" json:"label"`
+	X     int    `bson:"x" json:"x"`
+	Y     int    `bson:"y" json:"y"`
+}
+
+type RectAnnotation struct {
+	Label  string `bson:"label" json:"label"`
+	X      int    `bson:"x" json:"x"`
+	Y      int    `bson:"y" json:"y"`
+	Width  int    `bson:"width" json:"width"`
+	Height int    `bson:"height" json:"height"`
+}
+
+func (ra *RectAnnotation) Rectangle() image.Rectangle {
+	min := image.Point{ra.X, ra.Y}
+	max := image.Point{ra.X + ra.Width, ra.Y + ra.Height}
+	return image.Rectangle{Min: min, Max: max}
+}
+
+type Annotation struct {
+	Id   int    `bson:"id" json:"id"`
+	Type string `bson:"type" json:"type"`
+
+	// Exclusive fields
+	Rect  *RectAnnotation  `bson:"rect,omitempty" json:"rect,omitempty"`
+	Point *PointAnnotation `bson:"point,omitempty" json:"point,omitempty"`
+}
+
+type AnnotationCollection []Annotation
+
+func (ac AnnotationCollection) PointAnnotations() []PointAnnotation {
+	var ps []PointAnnotation
+	for _, a := range ac {
+		if a.Point != nil {
+			ps = append(ps, *a.Point)
+		}
+	}
+	return ps
+}
+
+func (ac AnnotationCollection) RectAnnotations() []RectAnnotation {
+	var rs []RectAnnotation
+	for _, a := range ac {
+		if a.Rect != nil {
+			rs = append(rs, *a.Rect)
+		}
+	}
+	return rs
+}
+
+func FindPointAnnotationRect(label string, annots AnnotationCollection, padding int, image image.Image) RectAnnotation {
+	bounds := image.Bounds()
+	minx := math.MaxInt64
+	miny := math.MaxInt64
+	maxx := 0
+	maxy := 0
+	for _, annot := range annots {
+		if annot.Point == nil {
+			continue
+		}
+		minx = min(minx, annot.Point.X)
+		miny = min(minx, annot.Point.Y)
+
+		maxx = max(maxx, annot.Point.X)
+		maxy = max(maxy, annot.Point.Y)
+	}
+	return RectAnnotation{
+		Label:  label,
+		X:      max(minx-padding, 0),
+		Y:      max(miny-padding, 0),
+		Width:  min(maxx-minx+padding, bounds.Max.X),
+		Height: min(maxy-miny+padding, bounds.Max.Y),
+	}
+}
+
+func PointToAnnotation(points []pts.Point) []Annotation {
+	var annotations []Annotation
+	index := 0
+	for _, v := range points {
+		annotations = append(annotations, Annotation{
+			Id:   index,
+			Type: "point",
+			Point: &PointAnnotation{
+				Label: strconv.Itoa(index),
+				X:     v.X,
+				Y:     v.Y,
+			},
+		})
+		index++
+	}
+	return annotations
+}
+
+func AnyRectAnnotation(annots []Annotation) bool {
+	for _, a := range annots {
+		if a.Rect != nil {
+			return true
+		}
+	}
+	return false
+}
+
+func AnyPointAnnotation(annots []Annotation) bool {
+	for _, annot := range annots {
+		if annot.Point != nil {
+			return true
+		}
+	}
+	return false
+}
