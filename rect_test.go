8cd035dc72c55eae2bbd1929cb24a3961d339b74
Evan Lin <evanslin@gmail.com>
2018-07-06 01:30:17 +0800

    fix  rectangle logic and testing.

--- rect_test.go
+++ rect_test.go
@@ -1,9 +1,10 @@
 package annotation
 
 import (
-	"github.com/stretchr/testify/assert"
 	"image"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRectCanonicalize(t *testing.T) {
@@ -19,6 +20,21 @@ func TestRectCanonicalize(t *testing.T) {
 	assert.Equal(t, 10, r.Y)
 	assert.Equal(t, 100, r.Width)
 	assert.Equal(t, 90, r.Height)
+}
+
+func TestRectRectangle(t *testing.T) {
+	r := RectAnnotation{
+		Label:  "test",
+		X:      100,
+		Y:      100,
+		Width:  -100,
+		Height: -90,
+	}
+	rect := r.Rectangle()
+	assert.Equal(t, 0, rect.Min.X)
+	assert.Equal(t, 10, rect.Min.Y)
+	assert.Equal(t, 100, rect.Max.X)
+	assert.Equal(t, 100, rect.Max.Y)
 }
 
 func TestRectFixBounds(t *testing.T) {
